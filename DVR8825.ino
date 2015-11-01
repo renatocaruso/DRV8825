1f26ee5cca1b15abb00bc464f02907830e87250e
renatocaruso <renatocarusos@hotmail.com>
2015-11-01 21:22:43 -0200

    Create DVR8825.ino

---
+++ DVR8825.ino
@@ -0,0 +1,18 @@
+#include "DVR8825.h"
+                                       // DVR8825 motorA ( pin_passo, pin_direção );   ou 
+DVR8825 motorA(1, 2, 10, 11, 12, 13);  // DVR8825 motorA ( pin_passo, pin_direção, pin_mo0, pin_mo1, pin_mo2, pin_sleep );
+
+void setup() {
+
+}
+
+void loop() {
+  motorA.marche(0, 0);                //  motorA.marche ( horario, passo_cheio ); 
+                                      //     "  .   "   ( 1 = !horario, 1 = 1/2 );
+                                      //                                2 = 1/4
+                                      //                                3 = 1/8
+                                      //                                4 = 1/16
+                                      //                                5 = 1/32                        
+motorA.desativa();  //  corta a energia das bobinas pra deixar o motor 'frouxo' (sem torque de retenção)
+motorA.ativa();     //  retoma
+}
